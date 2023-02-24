919fe426118dfaca597cf9477684f98af010ac90
ojokne <ojokne@gmail.com>
2023-02-24 09:05:45 +0300

    Update actions.js

--- src/context/actions.js
+++ src/context/actions.js
@@ -1,6 +1,4 @@
 export const ACTIONS = {
-  AUTHENTICATE: "authenticate user",
-  LOGOUT: "Log out user",
   ADD_QUOTE: "Add a quote",
   CLEAR_QUOTE: "Clear a quote",
   ADD_ORDERS: "Add all orders fetched",
